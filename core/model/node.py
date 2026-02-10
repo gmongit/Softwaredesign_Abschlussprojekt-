d6360acfbcad8425a9266d0dac15eca045a9e391
nsextro-code <nsextro@gmail.com>
2026-02-10 22:26:09 +0100

    Update node.py

--- core/model/node.py
+++ core/model/node.py
@@ -1,13 +1,1 @@
-from dataclasses import dataclass
 
-@dataclass
-class Node:
-    id: int
-    x: float
-    y: float
-
-    fx: float = 0.0
-    fy: float = 0.0
-
-    fixed_x: bool = False
-    fixed_y: bool = False
